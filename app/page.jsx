711f242dca7db908a7bac0383894724010895683
bo.brugghe <bo.brugghe@student.howest.be>
2023-05-10 19:38:12 +0200

    deleteing of conosle.log

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,6 @@ import BlogPost from "@/components/BlogPost";
 import data from "../data/blogposts.js";
 
 export default function Home() {
-  console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="max-w-screen-lg">
