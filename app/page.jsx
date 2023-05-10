86462dfef0c9cfccd959fd07280bd7463a971ca3
bo.brugghe <bo.brugghe@student.howest.be>
2023-05-10 20:29:20 +0200

    added simple title to the page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import BlogPost from "@/components/BlogPost";
 import data from "../data/blogposts.js";
+import MainTitle from "@/components/MainTitle.jsx";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
+      <MainTitle />
       <div className="max-w-screen-lg">
         {data.map(post => {
           return <BlogPost post={post} />;
---
+++ components/MainTitle.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MainTitle() {
+  return (
+    <div className="w-full h-50">
+      <h1 className="text-6xl font-bold text-center mt-16">
+        International Project Eindhoven
+      </h1>
+    </div>
+  );
+}
+
+export default MainTitle;
