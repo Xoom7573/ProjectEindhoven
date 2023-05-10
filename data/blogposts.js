da28abf6ca9c06625392a574b3e3076a336bcf76
bo.brugghe <bo.brugghe@student.howest.be>
2023-05-10 20:10:25 +0200

    Update blog data

--- data/blogposts.js
+++ data/blogposts.js
@@ -3,67 +3,57 @@ const posts = [
     title: "Sunday",
     date: "May 7 2023",
     alineas: [
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum"
+      "As part of an exciting international project for school, we arrived on Sunday evening at around 8 pm, filled with anticipation for the adventure ahead. After checking into our accommodation and unpacking our bags, we had the pleasure of meeting our fellow project members. It was fascinating to learn about their backgrounds and cultures, and we exchanged stories and ideas that helped us get to know each other better.",
+      "To get a feel for the local scene, we decided to take a walk to the city center. It was a 30-minute walk, but it was worth it for the beautiful sights along the way. As we reached the city, we decided to visit a local bar and grab some drinks. The atmosphere was vibrant, and we soaked up the energy and excitement of the city.      ",
+      "Over our drinks, we discussed our project goals and plans for the coming days. It was inspiring to hear everyone's ideas and to see how we could work together to achieve our objectives.",
+      "All in all, it was an incredible start to our international project, and we're looking forward to what the rest of our time here will bring. Keep following our journey for more updates!"
     ]
   },
   {
     title: "Monday",
     date: "May 8 2023",
     alineas: [
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum"
+      `Today was the official start of our international project, and we were excited to meet our teacher who would lead us on this adventure. After a brief introduction, we dove right into a lecture challenge called the "Smombie Run Challenge." It was a fun way to kick off the day, and it got us moving and energized.`,
+      "Next, we received an introduction to the project and met our team members. We spent some time brainstorming ideas and getting to know each other's strengths and interests. It was fascinating to see the different perspectives and ideas that emerged from our discussions.",
+      "During lunch, we enjoyed a variety of delicious sandwiches, fueling us up for the next part of the day. We had a guest lecture on future technologies, including the exponential growth of computing power and the potential impact on employment. It was eye-opening to think about how technology will shape our future.",
+      "After the lecture, we had the opportunity to talk about our ideas for the Smombie Run Challenge. We shared our unique perspectives and ideas on how to approach the challenge, and it was inspiring to see the different approaches and creativity from everyone.",
+      "Later in the day, we had the chance to present and pitch our ideas to the group. It was nerve-wracking, but also a great learning experience. After that, we had a cultural tasting, which allowed us to sample some of the local cuisine and learn more about the culture.",
+      "In the evening, we decided to eat in the hotel and relax after a busy day. Some of us ventured out to explore the city, while others opted for a more leisurely evening in. It was a great way to end the day, and we're excited to see what the rest of the trip has in store for us."
     ]
   },
   {
     title: "Thuesday",
     date: "May 9 2023",
     alineas: [
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum"
+      "On Day 2 of our international project, we started the day by going over the program and what we could expect to learn and accomplish. We were excited to dive into another day of learning and collaboration.",
+      "Our first lecture and workshop was about concepting, and it was fascinating to learn about the most important aspects of developing a unique concept. We learned about the importance of having a unique selling point and finding ways to satisfy our users. We also learned about the value of user testing to validate our ideas, as well as the role of role-playing in presenting concepts to stakeholders.",
+      "After the lecture, we worked on our concept ideas, building on what we had learned and discussed earlier. Later in the day, we had a lecture on game design and game mechanics. It was interesting to see how game design principles could apply to other areas of life and business.",
+      "We then had the opportunity to work on a game design document with our teams, and continued working on developing our concepts further. We had pre-prepared sandwiches for lunch and continued working throughout the afternoon.",
+      "Towards the end of the day, we had a wrap-up presentation to share our progress and get feedback from our teacher and fellow students. It was exciting to see how much progress we had made in just two days!",
+      "To end the day on a high note, we were treated to delicious street food in the form of Mexican street rolls. It was the perfect way to relax and unwind after a busy day. We can't wait to see what tomorrow brings!"
     ]
   },
   {
     title: "Wednesday",
     date: "May 10 2023",
     alineas: [
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum"
+      "Day 3 of our international project was another exciting day filled with learning and collaboration. We started the day with a kick-off meeting at 9 am.",
+      "The first order of business was pitch concept presentations to our teacher, Rens. We were all eager to share our ideas and see what feedback we would receive. After presenting our concepts, we received valuable feedback from Rens on how we could improve and refine our ideas.",
+      "We then spent the rest of the morning working on improving our concepts and incorporating the feedback we had received. We were all dedicated to making our concepts the best they could be.",
+      "For lunch, we were treated to delicious wraps, which we enjoyed while continuing to work on our concepts.",
+      "In the afternoon, we had a wrap-up presentation where we had the opportunity to show a small demo of what our app would look like. It was exciting to see how much progress we had made in just a few days.",
+      "To end the day on a high note, we were treated to tasty French fries from a food truck. We all gathered around and enjoyed our fries while reflecting on the progress we had made so far. It was another successful day of learning and collaboration, and we couldn't wait to see what tomorrow would bring."
     ]
   },
   {
     title: "Thursday",
     date: "May 11 2023",
-    alineas: [
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum"
-    ]
+    alineas: ["lorem ipsum", "lorem ipsum"]
   },
   {
     title: "Friday",
     date: "May 12 2023",
-    alineas: [
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum",
-      "lorem ipsum"
-    ]
+    alineas: ["lorem ipsum", "lorem ipsum"]
   }
 ];
 
---
+++ data/international.md
@@ -0,0 +1,55 @@
+# day 0 aankomst
+
+- inchecken en uitpakken
+- meet other people
+- going out for a drink to the city (30 min walk)
+
+# day 1 first day
+
+- introduction
+- lecture challenge; 
+- introduction project; meeting team
+  - brainstorming ideas
+- lunch (variety of sandwiches)
+- guest lecture -> future technologie
+  - humans need apply
+  - exponential grow
+- talking about your idea
+- Presentations/pitch your idea
+- culturale tasting
+- ate in hotel / going to ah; explored the city or walked around a bit (We just stayed in this night)
+
+# Day 2
+- morning and going over program
+- lecture/workshop concepting ; short introduction concepting
+  - most important thing unique selling point -> what makes you different from others
+  - find a way to satisfy your users
+  - usertesting to validate your idea
+  - role-play to get a clear view
+- working on concept
+- lecture game design
+  - you don't need it but it can help you (make it faster)
+  - entering magic cirle
+- making of a game design document with your team
+  - and futher working on your concept/idea
+- lunch (stuten)
+- working more with your groups
+- around 15u30 wrap-ups
+- end of the day we're offered streetfood (mexican streetrolls)
+
+# Day 3
+
+- kick-off 9u
+- pitch concept presentations 9u05 @ rens
+  - getting feedback for our client
+- working ! 
+- lunch (wraps)
+- 15u30 wrap-up
+- end of the day we're offered fries from a frietkar
+
+# Day 4
+
+
+
+# Day 5
+
