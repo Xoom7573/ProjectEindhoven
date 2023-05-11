ace229965f9e649bbce7de783807346d277b2da3
bo.brugghe <bo.brugghe@student.howest.be>
2023-05-11 08:59:29 +0200

    added padding

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <MainTitle />
-      <div className="max-w-screen-lg">
+      <div className="max-w-screen-lg pl-5 pr-5">
         {data.map(post => {
           return <BlogPost post={post} />;
         })}
