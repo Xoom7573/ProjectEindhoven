e0b3553608c59a69dbe0345a8d5f335bf23c6490
bo.brugghe <bo.brugghe@student.howest.be>
2023-05-15 23:32:12 +0200

    added blog animation

--- components/BlogPost.jsx
+++ components/BlogPost.jsx
@@ -1,26 +1,37 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
 
+import { motion } from "framer-motion";
+
 function BlogPost({ post }) {
   return (
-    <div className="mt-20 mb-20">
-      <h2>{post?.date}</h2>
-      <h1 className="text-4xl font-bold mb-8">{post?.title}</h1>
+    <motion.div
+      initial={{ opacity: 0, x: -250 }}
+      whileInView={{ opacity: 1, x: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="mt-20 mb-20">
+        <h2>{post?.date}</h2>
+        <h1 className="text-4xl font-bold mb-8">{post?.title}</h1>
 
-      <Image
-        className="rounded-lg mb-6 h-[30rem] w-full object-cover"
-        src={post?.image}
-        width={1920}
-        height={1080}
-        alt="sfeer foto"
-        objectFit="cover"
-        title="sfeer foto"
-      />
+        <Image
+          className="rounded-lg mb-6 h-[30rem] w-full object-cover"
+          src={post?.image}
+          width={1920}
+          height={1080}
+          alt="sfeer foto"
+          objectFit="cover"
+          title="sfeer foto"
+        />
 
-      {post?.alineas.map(alinea => {
-        return <p className="text-lg text-justify mb-5">{alinea}</p>;
-      })}
-    </div>
+        {post?.alineas.map(alinea => {
+          return <p className="text-lg text-justify mb-5">{alinea}</p>;
+        })}
+      </div>
+    </motion.div>
   );
 }
 
