ba495880fd982307a91e89eefc4adb3c8e7f8789
bo.brugghe <bo.brugghe@student.howest.be>
2023-05-22 11:02:34 +0200

    update day Don & Vrij

--- data/blogposts.js
+++ data/blogposts.js
@@ -7,8 +7,8 @@ const posts = [
       "As part of an exciting international school project, we arrived on Sunday evening at around 8 pm. We were excited about the adventure to come. After checking into the Blue Collar Hotel and unpacking our baggage, we had the chance of meeting our other project members. It was a nice experience to learn more about their backgrounds and cultures. We talked a lot about how our countries and mentalities differ.",
       "We wanted to explore Eindhoven, so we decided to take a walk to the city center. It was a 30-minute walk, but there were lots of nice sightings along the way. As we reached the city, we decided to visit a local bar and grab some drinks.",
       "While drinking our beers, we discussed our project goals and plans for the rest of the week. We were all excited about to work together.",
-      "After all, it was an nice start to our international project. We are all looking forward to seeing what the rest of the week has to offer."
-    ]
+      "After all, it was an nice start to our international project. We are all looking forward to seeing what the rest of the week has to offer.",
+    ],
   },
   {
     title: "Monday - A Promising Start",
@@ -20,8 +20,8 @@ const posts = [
       "During lunch, we ate a variety of delicious sandwiches. This give us energy for the rest of the day. We had a guest lecture on future technologies, the lecture was about the impact of computing power. It was eye-opening to think about how technology will shape our future.",
       "After the lecture, we had the opportunity to talk about our ideas for the Smombie Run Challenge. We shared our unique ideas on how to approach the challenge. It was amazing to see everyone's ideas.",
       "Later in the day, we had the chance to present and pitch our ideas to the group. It was nerve-wracking, but also a great learning experience. After that, we had a cultural tasting, we were able to try some of the local cuisine and learn more about the culture.",
-      "In the evening, we decided to eat in the hotel and relax after a busy day. Some of us ventured out to explore the city, while others just staid at the hotel. It was a great way to end the day, and we're excited to see what the rest of the trip has in store for us."
-    ]
+      "In the evening, we decided to eat in the hotel and relax after a busy day. Some of us ventured out to explore the city, while others just staid at the hotel. It was a great way to end the day, and we're excited to see what the rest of the trip has in store for us.",
+    ],
   },
   {
     title: "Tuesday - Concept Development and Game Design",
@@ -33,8 +33,8 @@ const posts = [
       "After the lecture, we had an hour to work on and improve our concept ideas with the knowledge we learned and discussed earlier, after that we had another interesting lecture.",
       "The second lecture was about game design and game dynamics. We learned that you don't necessarily need a game design to design a great game, but that I can be very helpful.",
       "They taught us some game design lingo like setting, theme, magic circle... During the lecture, they also told us how to design a game. How you start with a one-pager that grows into a 10-pager and beyond.",
-      "After the lecture about game design, we had the opportunity to work on the game design document with our group. We made a one-pager document about our concept, and after lunch we developed our concept further."
-    ]
+      "After the lecture about game design, we had the opportunity to work on the game design document with our group. We made a one-pager document about our concept, and after lunch we developed our concept further.",
+    ],
   },
   {
     title: "Wednesday - Concept Refinement and Inspiring Demonstrations",
@@ -46,37 +46,50 @@ const posts = [
       "With the feedback from Rens we refined and improved our ideas. We were dedicated to make our concepts the best they could be.",
       "For lunch, we had wraps and couscous with the whole group of the international week.",
       "After lunch, we did a small wrap-up presentation where we could show a small demo of what our app would look like.",
-      "To end the day, we were treated to tasty French fries from a food truck. We gathered around and enjoyed our fries while reflecting on the progress we made so far."
-    ]
+      "To end the day, we were treated to tasty French fries from a food truck. We gathered around and enjoyed our fries while reflecting on the progress we made so far.",
+    ],
   },
   {
     title: "Thursday - Feedback, and Exploring Eindhoven",
     date: "May 11 2023",
     image: "/assets/racing.jpg",
     alineas: [
-      "Day 4 of our international project week in Eindhoven was a busy one. We had been working hard on our projects for the past few days and it was time to finalize and test them before presenting to the jury.",
-      "In the morning, we focused on putting the final touches on our projects and conducted thorough testing of our applications. It was a critical step to ensure that our projects were functioning correctly and met our client's requirements.",
-      "At 11 am, we had our final wrap-up presentation. We presented our projects to our fellow participants and received valuable feedback from them. It was an opportunity to showcase our projects and get a sense of what others had been working on.",
-      "After the presentation, we took a much-needed lunch break to refuel and recharge. We enjoyed a variety of delicious sandwiches and discussed our projects with our teams.",
-      "In the afternoon, we had the chance to explore the city of Eindhoven with a guided tour. We learned about the city's history, culture, and architecture. We also had the opportunity to see some of Eindhoven's famous landmarks and museums, such as the Van Abbemuseum, the Philips Museum, and the Strijp-S area.",
-      "The tour was a great way to take a break from our projects, learn more about the city we were in, and bond with our fellow participants.",
-      "Overall, day 4 was a great mix of work and play. We were able to finalize our projects, receive feedback, and explore the beautiful city of Eindhoven. It was a day to remember and a great way to cap off an unforgettable week."
-    ]
+      "Day 4 of our international project week in Eindhoven was busy.",
+      "We had been working hard on our projects for the past few days and it was time to finalize and test everything before presenting it to the jury.",
+      "We focused on putting the final touches on our projects and we got to go outside to test our applications and see how it influenced our behavior.",
+      "It was a critical step to ensure that our projects were functioning correctly and met our client's requirements.",
+      "At 11 am, we had our final and last wrap-up presentation.",
+      "We presented our projects to our fellow participants and received valuable feedback from them.",
+      "After the presentation, we took a lunch break and enjoyed a variety of delicious sandwiches.",
+      "In the afternoon, we had the chance to explore the city of Eindhoven with a guided tour.",
+      "We learned about the city's history, culture, and architecture.",
+      "We also had the opportunity to see some of Eindhoven's famous landmarks and museums, such as the Van Abbemuseum, the Philips Museum, and the Strijp-S area.",
+      "The tour was a great way to take a break from our projects and bond with our fellow participants.",
+      "Overall we were able to finalize our projects, receive feedback and explore the beautiful city of Eindhoven.",
+    ],
   },
   {
     title: "Friday - Final Presentations and Farewell",
     date: "May 12 2023",
     image: "/assets/sfeer.jpg",
     alineas: [
-      "Day 5 of our international project week in Eindhoven was bittersweet. We had spent the past few days working hard on our projects, collaborating with our teams, and learning from experts in various fields. But it was also the day we had to say goodbye to our new friends and colleagues.",
-      "We started the day with our usual daily start at 9 am, but this time it was focused on finalizing our projects and preparing for our group presentations to the jury.",
-      "At 10:30, it was time for us to present our projects to the jury. We were all nervous but excited to share our ideas with them. Each group presented their project, showcasing their unique selling point and how they had addressed their users' needs. It was impressive to see how much progress we had made in just a few short days.",
-      "After the presentations, we had a well-deserved lunch break to relax and enjoy some food together. It was a great opportunity to debrief with our teams and reflect on the past week's experiences.",
-      "At 1 pm, we gathered for the award ceremony and received our certificates. It was a proud moment for all of us to see our hard work pay off and be recognized by the experts and our peers.",
-      "The day ended with closing remarks and goodbyes. It was a bit sad to say goodbye to our new friends and colleagues, but we were grateful for the opportunity to have worked together and learned from each other. We left Eindhoven with new skills, experiences, and memories that we will cherish forever.",
-      "Overall, the international project week in Eindhoven was a fantastic experience that pushed us out of our comfort zones and allowed us to grow both personally and professionally. We were grateful for the opportunity to participate and are excited to see where our newfound knowledge and skills will take us in the future."
-    ]
-  }
+      "Our last day of our international project week in Eindhoven.",
+      "We started the day with our usual daily start at 9 am, but this time it was focused on finalizing our projects.",
+      "At 10:30, it was time for us to present our projects to the jury.",
+      "We're all nervous but excited to share our ideas with them.",
+      "Each group presented their project, showcasing their unique selling point and how they had addressed their users' needs.",
+      "It was impressive to see how much progress we had made.",
+      "After the presentations, we had a well-deserved lunch break to relax and enjoy some food together.",
+      "It was a great opportunity to talk with our teams and reflect on the past week's experiences.",
+      "At 1 pm, we gathered for the award ceremony and received our certificates.",
+      "It was a proud moment for all of us to see our hard work pay off and be recognized by the experts and our peers.",
+      "The day ended with goodbyes.",
+      "It was a bit sad to say goodbye to our new friends and colleagues, but we were grateful for the opportunity to have worked together and learned from each other.",
+      "We left Eindhoven with new skills, experiences, and memories that we will cherish forever.",
+      "Overall, the international project week in Eindhoven was a fantastic experience that pushed us out of our comfort zones and allowed us to grow both personally and professionally.",
+      "We were grateful for the opportunity to participate and are excited to see where our newfound knowledge and skills will take us in the future.",
+    ],
+  },
 ];
 
 export default posts;
